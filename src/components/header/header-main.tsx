36121ab970abc547923bde99a625e472defedb12
Alena Aleksenko <arslanbekova@gmail.com>
2024-07-23 20:06:07 +0300

    Temporarily removed logo

--- src/components/header/header-main.tsx
+++ src/components/header/header-main.tsx
@@ -1,16 +1,13 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import LocationsList from '../cities-list/cities-list';
-import Logo from '../logo/logo';
 
 function HeaderMain(): JSX.Element {
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
-          <div className="header__left">
-            <Logo />
-          </div>
+          <div className="header__left"></div>
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
